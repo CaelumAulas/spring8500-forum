cddc18a2f317eaf54db95aaf3547b4f6ec4bf45c
Ana Losnak <ana.losnak@caelum.com.br>
2019-08-19 16:48:21 -0300

    ex 5.5 - Listando topicos do banco

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,30 +1,25 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.TopicRepository;
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
-import br.com.alura.forum.model.Category;
-import br.com.alura.forum.model.Course;
-import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
 
 @RestController
 public class TopicController {
+	@Autowired
+	private TopicRepository topicRepository;
+	
 	@GetMapping(value="/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<TopicBriefOutputDto> listaTopicos() {
-		User owner = new User("Ana", "a@b.c", "1234");
-		Category category = new Category("Programação");
-		Category subCategory = new Category("Java", category);
-		Course course = new Course("Aprendendo Spring MVC", subCategory);
-		Topic topic = new Topic("como estudar spring?",
-				"bom dia, não estou conseguindo estudar", owner, course);
 		
-		List<Topic> topics = Arrays.asList(topic, topic, topic);
+		List<Topic> topics = topicRepository.findAll();
 		
 		return TopicBriefOutputDto.listFromTopics(topics);
 	}
