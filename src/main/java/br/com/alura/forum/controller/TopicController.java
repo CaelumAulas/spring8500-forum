9b3f09ff606a32e74cd1230dbe34ae0404bd1d03
Ana Losnak <ana.losnak@caelum.com.br>
2019-08-20 10:41:43 -0300

    ex 5.5 - Criando queries dinâmicas

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -3,23 +3,26 @@ package br.com.alura.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.TopicRepository;
+import br.com.alura.forum.controller.dto.input.TopicSearchInputDto;
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.repository.TopicRepository;
 
 @RestController
 public class TopicController {
 	@Autowired
 	private TopicRepository topicRepository;
-	
-	@GetMapping(value="/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<TopicBriefOutputDto> listaTopicos() {
+
+	@GetMapping(value = "/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<TopicBriefOutputDto> listaTopicos(TopicSearchInputDto inputDto) {
+		Specification<Topic> specification = inputDto.buildSpecification();
 		
-		List<Topic> topics = topicRepository.findAll();
+		List<Topic> topics = topicRepository.findAll(specification);
 		
 		return TopicBriefOutputDto.listFromTopics(topics);
 	}
---
+++ src/main/java/br/com/alura/forum/controller/dto/input/TopicSearchInputDto.java
@@ -0,0 +1,58 @@
+package br.com.alura.forum.controller.dto.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.model.topic.domain.TopicStatus;
+
+public class TopicSearchInputDto {
+	private TopicStatus status;
+	private String categoryName;
+	
+	public TopicStatus getStatus() {
+		return status;
+	}
+	public void setStatus(TopicStatus status) {
+		this.status = status;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public Specification<Topic> buildSpecification() {
+		Specification<Topic> specification = new Specification<Topic>() {
+			@Override
+			public Predicate toPredicate(Root<Topic> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+			List<Predicate> predicates = new ArrayList<>();
+				
+				if (status != null) {
+					Predicate predicateStatus = criteriaBuilder.equal(root.get("status"), status);
+					predicates.add(predicateStatus);
+				}
+				
+				if (categoryName != null) {
+					Predicate predicateCategoryName = 
+							criteriaBuilder.equal(
+									root.get("course").get("subcategory").get("category").get("name"), 
+									categoryName);
+					predicates.add(predicateCategoryName);
+				}
+				
+				Predicate predicate = criteriaBuilder.and(predicates.toArray(new Predicate[]{}));
+				
+				return predicate;
+			}
+		};
+		return specification;
+	}
+}
